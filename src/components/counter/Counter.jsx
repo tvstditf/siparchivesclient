42395d06f5afd6616191ce25905e3750a88676ad
papijo <joe.ebh100@live.com>
2022-07-31 14:50:32 +0100

    Minor Change to Counter

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -46,8 +46,8 @@ const Counter = () => {
     const getNumber = async () => {
       try {
         const resCen = await userRequest.get(`/centre/?`);
-        setCentres(resCen?.data);
         const resTrain = await userRequest.get(`/trainee/?`);
+        setCentres(resCen?.data);
         setTrainees(resTrain?.data);
       } catch (error) {
         // console.log(error);
