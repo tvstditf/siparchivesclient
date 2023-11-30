930e5e30c2a8661a8adaad639bc36b691ee6dbac
Ebhota Jonathan <joe.ebh100@live.com>
2023-11-30 10:25:10 +0100

    Changes to baseurl

--- src/utils/requestMethods.js
+++ src/utils/requestMethods.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+// const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+const BASE_URL = "https://sip-databank-api-b6375c164739.herokuapp.com/api/";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
